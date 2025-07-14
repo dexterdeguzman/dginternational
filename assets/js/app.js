94d49a49a1d7ac05af20d4de984a22fc3efe5dbb
Dexter De Guzman <dexterdeguzman@Dexters-MacBook-Air.local>
2025-07-14 18:51:16 +0800

    Initial page

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+	// wow = new WOW(
+	// 	{
+	// 		animateClass: 'animated',
+	// 		offset: 100
+	// 	}
+	// );
+	// wow.init();
+
+	// var menuBtn = $('.etoNavIcon');
+
+	// menuBtn.click(function () {
+	// 	$(this).toggleClass("-showClose");
+	// });
+});
